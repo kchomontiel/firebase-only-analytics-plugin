74970b5e13d2317239bea5c5f30e12a4bd2df87c
everis.jean.carlo <everis.jean.carlo>
2024-07-30 08:59:58 -0500

    uncomment notifications

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -3,7 +3,7 @@
 #import "AppDelegate.h"
 // @import Fabric;
 // @import Crashlytics;
-//@import FirebaseInstanceID; --Commented 25/072024
+@import FirebaseInstanceID;
 @import FirebaseMessaging;
 @import FirebaseAnalytics;
 // @import FirebaseRemoteConfig;
@@ -43,8 +43,7 @@ static FirebasePlugin *firebasePlugin;
 // Notifications
 //
 
-//Commented 25/07/2024
-/*
+
 - (void)getId:(CDVInvokedUrlCommand *)command {
   __block CDVPluginResult *pluginResult;
 
@@ -175,7 +174,7 @@ static FirebasePlugin *firebasePlugin;
                 CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
             }];
-            */ //Commented 25/07/2024
+            
 
         /*
             NSString* currentToken = [[FIRInstanceID instanceID] token];
@@ -185,13 +184,12 @@ static FirebasePlugin *firebasePlugin;
             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
             */
- /* //Commented 25/07/2024
+
         }
     }];
 }
-*/ //Commented 25/07/2024
 
-/* //Commented 25/07/2024
+
 - (void)onNotificationOpen:(CDVInvokedUrlCommand *)command {
     self.notificationCallbackId = command.callbackId;
 
@@ -216,7 +214,7 @@ static FirebasePlugin *firebasePlugin;
     }];
 }
 
-*/ //Commented 25/07/2024
+
 /*MODIFIED
 - (void)onTokenRefresh:(CDVInvokedUrlCommand *)command {
     self.tokenRefreshCallbackId = command.callbackId;
@@ -228,7 +226,6 @@ static FirebasePlugin *firebasePlugin;
 }*/
 
 
-/* //Commented 25/07/2024
 - (void)sendNotification:(NSDictionary *)userInfo {
     if (self.notificationCallbackId != nil) {
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userInfo];
@@ -265,8 +262,6 @@ static FirebasePlugin *firebasePlugin;
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
-
-*/ //Commented 25/07/2024
 
 
 //
