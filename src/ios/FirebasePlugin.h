bb8084e148eea0d0b4f5b927fbe47121d2b5b48e
Julio Alfredo Pardo Morales <julio.pardo@entel.pe>
2025-09-12 23:56:15 +0000

    FirebasePlugin.h editado a través de la web en Bitbucket

--- src/ios/FirebasePlugin.h
+++ src/ios/FirebasePlugin.h
@@ -18,19 +18,9 @@
 - (void)sendNotification:(NSDictionary *)userInfo;
 - (void)sendToken:(NSString *)token;
 - (void)logEvent:(CDVInvokedUrlCommand *)command;
-// - (void)logError:(CDVInvokedUrlCommand *)command;
-// - (void)setCrashlyticsUserId:(CDVInvokedUrlCommand*)command;
 - (void)setScreenName:(CDVInvokedUrlCommand *)command;
 - (void)setUserId:(CDVInvokedUrlCommand *)command;
 - (void)setUserProperty:(CDVInvokedUrlCommand *)command;
-// - (void)activateFetched:(CDVInvokedUrlCommand *)command;
-// - (void)fetch:(CDVInvokedUrlCommand *)command;
-// - (void)getValue:(CDVInvokedUrlCommand *)command;
-// - (void)startTrace:(CDVInvokedUrlCommand *)command;
-// - (void)incrementCounter:(CDVInvokedUrlCommand *)command;
-// - (void)stopTrace:(CDVInvokedUrlCommand *)command;
-// - (void)forceCrashlytics:(CDVInvokedUrlCommand *)command;
-// - (void)setPerformanceCollectionEnabled:(CDVInvokedUrlCommand*)command;
 - (void)setAnalyticsCollectionEnabled:(CDVInvokedUrlCommand *)command;
 - (void)clearAllNotifications:(CDVInvokedUrlCommand *)command;
 
