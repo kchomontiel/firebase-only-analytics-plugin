f708e8fa68fc86d30086ecace81fc09544876a44
Carlos <kchomontiei@gmail.com>
2025-09-15 20:01:54 -0300

    Fix IPHONEOS_DEPLOYMENT_TARGET variable reference in helper.js
    - Fixed line 167 in scripts/crashlytics/ios/helper.js
    - Changed from '' to '12.0'
    - This resolves the 'Variable(s) missing: DEPLOYMENT-TARGET' error
    - The script was trying to reference a variable that wasn't available in context
    - Now uses a fixed value that matches our plugin configuration

--- scripts/crashlytics/ios/helper.js
+++ scripts/crashlytics/ios/helper.js
@@ -164,7 +164,7 @@ module.exports = {
 post_install do |installer|
 installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '${IPHONEOS_DEPLOYMENT_TARGET}'
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
     end
 end
 end
