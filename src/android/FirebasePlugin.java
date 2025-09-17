52f2482f109a8718e16e04e65fe96ce32b7b7924
Carlos <kchomontiei@gmail.com>
2025-09-17 18:16:09 -0300

    Add missing FirebaseApp import
    - Add import com.google.firebase.FirebaseApp to FirebasePlugin.java
    - This fixes the compilation error: cannot find symbol FirebaseApp
    - FirebaseApp.initializeApp(context) can now be called properly
    - Should resolve the build failure

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
